897333a3e78d5b540545bfd52d634a33581b9968
Alex Bash <avbashlykov@gmail.com>
2017-07-30 11:50:48 +0200

    litecoin explorer changed

--- models/litecoin.py
+++ models/litecoin.py
@@ -1,12 +1,12 @@
 import aiohttp
-import json
 
 from .fetcher import Fetcher
+import logging
 
 
 class LitecoinAPI(Fetcher):
 
-    _URL = 'http://ltc.blockr.io/api/v1/address/balance/{}'
+    _URL = 'http://explorer.litecoin.net/chain/Litecoin/q/addressbalance/{}'
 
     async def get_ltc_balance(self, loop, address, symbol='LTC', callback=None):
         if address is None:
@@ -14,13 +14,11 @@ class LitecoinAPI(Fetcher):
         async with aiohttp.ClientSession(loop=loop) as session:
             endpoint = self._URL.format(address)
             response = await self._fetch(session, endpoint)
-            response = json.loads(response)
-            if response.get('status') != 'success':
-                print('LTC request failed:', response)
+            try:
+                amount = float(response)
+            except Exception:
+                logging.error('Litecoin balance fetching error')
                 return
-            balance = float(response.get('data').get('balance'))
-            multisig_balance = float(response.get('data').get('balance_multisig'))
-            amount = balance + multisig_balance
             if callback is not None:
                 callback(symbol, amount)
         return symbol, amount
